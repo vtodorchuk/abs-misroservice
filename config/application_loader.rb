7574ce249992c72ba1d8b1e09f55ff42205cfc76
vtodorchuk <bozodee8@gmail.com>
2025-03-10 14:10:15 +0200

    Add DB

--- config/application_loader.rb
+++ config/application_loader.rb
@@ -2,11 +2,21 @@ module ApplicationLoader
   extend self
 
   def load_app!
+    init_config
+    init_db
     require_app
     init_app
   end
 
   private
+
+  def init_config
+    require_file 'config/initializers/config'
+  end
+
+  def init_db
+    require_file 'config/initializers/db'
+  end
 
   def init_app
     require_dir 'config/initializers'
---
+++ config/initializers/db.rb
@@ -0,0 +1,10 @@
+DB = Sequel.connect(Settings.db.to_hash)
+
+Sequel::Model.db.extension(:pagination)
+
+Sequel::Model.plugin :validation_helpers
+Sequel::Model.plugin :timestamps, update_on_create: true
+
+Sequel.default_timezone = :utc
+
+
